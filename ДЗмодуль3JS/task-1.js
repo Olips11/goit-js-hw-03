7322c42cc6a7aef297675234edc17444fb09c585
Olips11 <66215413+Olips11@users.noreply.github.com>
2020-08-30 00:18:20 +0300

    1

---
+++ ДЗмодуль3JS/task-1.js
@@ -0,0 +1,16 @@
+function filterNumbers(array, ...args) {
+  const get = [];
+
+  for (const elemnt of array) {
+    if (args.includes(elemnt)) {
+      console.log(elemnt);
+
+      get.push(elemnt);
+    }
+  }
+
+  return get;
+}
+
+console.log(filterNumbers([1, 2, 3, 4, 5], 10, 15, 2, 8, 3));
+console.log(filterNumbers([1, 2, 3, 4, 5, 10, 15, 8], 10, 15, 2, 8, 3));
