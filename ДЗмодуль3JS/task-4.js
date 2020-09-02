faa42d33d4f1286b2a5b374261542121b2b18617
Chernysh Illya <olips1988@gmail.com>
2020-09-02 19:26:16 +0300

    1

---
+++ ДЗмодуль3JS/task-4.js
@@ -0,0 +1,42 @@
+// Суммирование значений свойств объекта
+// Напиши функцию countTotalSalary(employees) принимающую объект зарплат. 
+// Функция считает общую сумму зарплаты работников и возвращает ее. 
+// Каждое поле объекта, передаваемого в функцию, имеет вид "имя":"зарплата".
+
+
+
+
+const countTotalSalary = function(employees) {
+    'use strict';
+    // Write code under this line
+    let total = 0;;
+    
+  const userKeys = Object.keys(employees);
+//   console.log(userkeys);
+
+  for (const key of userKeys) {
+    // console.log(key);
+    // console.log(employees[key]);
+    total += employees[key]
+    // console.log(total);
+}    
+return total;
+  };
+  
+  // Объекты и ожидаемый результат
+  const developers = {
+      mango: 300,
+      poly: 250,
+      alfred: 450,
+  };
+  console.log(countTotalSalary(developers));
+  // 1000
+  
+  const supports = {
+    kiwi: 200,
+    lux: 150,
+    chelsy: 150,
+  }
+  console.log(countTotalSalary(supports));
+  // 500
+  
